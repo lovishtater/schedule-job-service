ea7c3b8b6df4aab424cdf48315f184ace781a887
Lovish Tater <lovish.t@source.one>
2024-07-30 00:52:58 +0530

    Initial commit with frontend and backend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+    <h1 className="text-3xl font-bold underline">
+      Hello
+    </h1>c
+  </div>
+  );
+}
+
+export default App;
